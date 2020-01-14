c273e3d34dbd2fe2c52588bc2185e732f25a77b4
Técnico em Informática 2019.6 <16121742019.6@senacrjedu.local>
2020-01-14 10:26:57 -0300

    humor

--- js/tamagoshi.js
+++ js/tamagoshi.js
@@ -1,3 +1,4 @@
+//codigo retirado do github https://raw.githubusercontent.com/fmoreirati/tamagoshi/master/js/tamagoshi.js
 var nome = "";
 var saude = 50;
 var fome = 50;
@@ -7,19 +8,21 @@ setInterval(display, 60000);//1000 = 1seg
 display()
 
 function display() {
-    tempo++
+    tempo++ //tempo = tempo + 1
     document.getElementById("tempo").innerHTML="Tempo: " + tempo
     document.getElementById("saude").innerHTML="Saude: " + saude
     document.getElementById("fome").innerHTML="Fome: " + fome
+    document.getElementById("humor").innerHTML="humor" + humor()
     imagem();
 }
 
 function humor() {
-   
+   return(fome + saude) / 2;
 }
 
 function comer(valor) {
-   
+    if(fome < 100){
+      fome = fome + valor 
 }
 
 function remedio(valor) {
@@ -27,7 +30,7 @@ function remedio(valor) {
 }
 
 function imagem() {
-    img = "t01.gif";
+   let img = "t01.gif";
     if (saude == 0 || fome == 0)
         img = "t07.gif";
     else if (tempo <= 4)
